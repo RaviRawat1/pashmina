0332fead429ab58b2b83f2f61fd9c7fda5a38313
Ravi Rawat <rawatcyberexpert@gmail.com>
2022-07-15 16:09:23 +0530

    image gallery changed

--- pages/productdetails.js
+++ pages/productdetails.js
@@ -224,7 +224,47 @@ export default function Example() {
           <div className="w-full container sm:mx-auto px-4 md:px-6 2xl:px-0 pt-0 sm:pt-14 pb-16 sm:pb-24 flex flex-col sm:flex-row gap-8">
             {/* Image gallery */}
 
-            
+            <Tab.Group as="div" className="flex flex-col-reverse">
+            {/* Image selector */}
+            <div className="hidden mt-6 w-full max-w-2xl mx-auto sm:block lg:max-w-none">
+              <Tab.List className="grid grid-cols-4 gap-6">
+                {product.images.map((image) => (
+                  <Tab
+                    key={image.id}
+                    className="relative h-24 bg-white rounded-md flex items-center justify-center text-sm font-medium uppercase text-gray-900 cursor-pointer hover:bg-gray-50 focus:outline-none focus:ring focus:ring-offset-4 focus:ring-opacity-50"
+                  >
+                    {({ selected }) => (
+                      <>
+                        <span className="sr-only">{image.name}</span>
+                        <span className="absolute inset-0 rounded-md overflow-hidden">
+                          <img src={image.src} alt="" className="w-full h-full object-center object-cover" />
+                        </span>
+                        <span
+                          className={classNames(
+                            selected ? 'ring-indigo-500' : 'ring-transparent',
+                            'absolute inset-0 rounded-md ring-2 ring-offset-2 pointer-events-none'
+                          )}
+                          aria-hidden="true"
+                        />
+                      </>
+                    )}
+                  </Tab>
+                ))}
+              </Tab.List>
+            </div>
+
+            <Tab.Panels className="w-full aspect-w-1 aspect-h-1">
+              {product.images.map((image) => (
+                <Tab.Panel key={image.id}>
+                  <img
+                    src={image.src}
+                    alt={image.alt}
+                    className="w-full h-full object-center object-cover sm:rounded-lg"
+                  />
+                </Tab.Panel>
+              ))}
+            </Tab.Panels>
+          </Tab.Group>
 
             {/* Product info */}
             <div className="sticky top-0 w-full sm:w-1/2">
