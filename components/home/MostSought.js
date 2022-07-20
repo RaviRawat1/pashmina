078ee9a9b5d54d6308678f923b91e0b7fe556703
Ravi Rawat <rawatcyberexpert@gmail.com>
2022-07-20 16:10:29 +0530

    push

--- components/home/MostSought.js
+++ components/home/MostSought.js
@@ -5,7 +5,7 @@ import styles from "../../styles/MostSought.module.css"
 export default function MostSought() {
     console.log(styles)
   return (
-    <section className={`${styles.wrap_full} ${styles.sought_pashmina_wrap}`}>
+    <section className={`${styles.wrap_full} ${styles.sought_pashmina_wrap} ${styles.temp_class}`}>
 		<div className={`${styles.box_container}`}>
 			<div className={`${styles.sought_pashmina} ${styles.content_size}`}>
 				<h2>
